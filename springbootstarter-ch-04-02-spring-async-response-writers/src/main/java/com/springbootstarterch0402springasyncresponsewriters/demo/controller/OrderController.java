0f56df692d6b92445ae0a3a8ec38f80bbe72caba
git <rramaswamy@psi.de>
2023-02-23 14:04:59 +0100

    Add API for SseEmiiter using EventBuilder

--- springbootstarter-ch-04-02-spring-async-response-writers/src/main/java/com/springbootstarterch0402springasyncresponsewriters/demo/controller/OrderController.java
+++ springbootstarter-ch-04-02-spring-async-response-writers/src/main/java/com/springbootstarterch0402springasyncresponsewriters/demo/controller/OrderController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import static org.springframework.web.servlet.mvc.method.annotation.SseEmitter.event;
 
 import com.springbootstarterch0402springasyncresponsewriters.demo.service.OrderService;
 
@@ -58,6 +59,27 @@ public class OrderController {
 				for (var order : orders) {
 					randomDelay();
 					emitter.send(order);
+				}
+				emitter.complete();
+			} catch (IOException e) {
+				emitter.completeWithError(e);
+			}
+		});
+		executor.shutdown();
+		return emitter;
+	}
+
+	@GetMapping("/orders/sse/events")
+	public SseEmitter orders() {
+		SseEmitter emitter = new SseEmitter();
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		executor.execute(() -> {
+			var orders = orderService.findAll();
+			try {
+				for (var order : orders) {
+					randomDelay();
+					var eventBuilder = event();
+					emitter.send(eventBuilder.data(order).name("order-created").id(String.valueOf(order.hashCode())));
 				}
 				emitter.complete();
 			} catch (IOException e) {
